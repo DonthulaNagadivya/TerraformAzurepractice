16971803fd6cf92f0a63736dbf7376e6aee2cf23
DonthulaNagadivya <nagadivya560@gmail.com>
2025-07-16 22:32:17 +0530

    import module

--- Data/main.tf
+++ Data/main.tf
@@ -1,13 +1,9 @@
 provider "azurerm" {
-  features { }
+  features {}
   subscription_id = "4c4a9b0e-3bf8-4ea2-a4d2-2fa272a13539"
 }
-data "azurerm_resource_group" "rg" {
-  name = "RG2"
+resource "azurerm_resource_group" "rg" {
+  name     = "RG2"
+  location = "East US"
 }
-resource "azurerm_virtual_network" "Vnet" {
-name = "vnet1"
-resource_group_name = data.azurerm_resource_group.rg.name
-location = data.azurerm_resource_group.rg.location
-address_space = ["192.168.0.0/16"]
-}
+ terraform import azurerm_resource_group.resourcegroup /subscriptions/subsriptionID/resourceGroups/RG2
